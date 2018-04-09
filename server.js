53c2e646e6b984f4c683a27f1f70ba6b4b423412
Cruz Molina <Des1derata@192.168.1.11>
2018-04-09 14:40:42 -0700

    Fixed dynamic routing

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { createServer } = require("http");
+/**const { createServer } = require("http");
 const next = require("next");
 
 const app = next({
@@ -11,6 +11,20 @@ const handler = routes.getRequestHandler(app);
 app.prepare().then(() => {
   createServer(handler).listen(3000, err => {
     if (err) throw err;
-    //    console.log("Ready on localhost:3000");
   });
+});**/
+
+const express = require("express");
+const next = require("next");
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const routes = require("./routes");
+const handler = routes.getRequestHandler(app);
+
+app.prepare().then(() => {
+  express()
+    .use(handler)
+    .listen(port);
 });
